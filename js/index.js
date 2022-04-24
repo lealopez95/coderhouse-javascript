bbda3cca1a2c4d096e3284e7c911c0e34a748b0a
Leandro Lopez <leandroe.lopez95@gmail.com>
2022-04-24 20:12:59 -0300

    add html project and simulator of cart functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,117 @@
-const name = window.prompt('Please enter your name:');
-let number = 0;
-let shouldContinue = true;
-while (shouldContinue) {
-    let answer = window.prompt('Please enter a number to accumulate. Type "ESC" to exit');
-    if(answer === "ESC" || answer ==="esc" || answer ==="Esc" ) {
-        shouldContinue = false;
-    } else {
-        number+= parseInt(answer);
-        window.alert("The result until now is: " + number);
+class Product {
+    price;
+    name;
+
+    constructor (name, price) {
+        this.price = price;
+        this.name = name;
     }
 }
-window.alert("good bye: " + name);
+
+class Cart {
+    products;
+    quantities;
+
+    constructor () {
+       this.products = []; 
+       this.quantities = {}; 
+    }
+
+    addProduct(product, qty) {
+        const result = this.products.findIndex( currProduct => currProduct.name === product.name );
+        if(result != -1) {
+            this.quantities[product.name] += qty;
+        } else {
+            this.products.push(product);
+            this.quantities[product.name] = qty;
+        }
+    }
+
+    getProducts = () => {
+        let products = [];
+        this.products.forEach( product => {
+            products.push({
+                name: product.name,
+                price: product.price,
+                qty: this.quantities[product.name],
+                total: this.quantities[product.name] * product.price,
+            })
+        });
+        return products;
+    }
+}
+
+validateYesNoPrompt = (message) => {
+    const retry = true;
+    while(retry) {
+        let answer = prompt(message);
+        if (!answer) {
+            alert("Por favor ingrese una respuesta valida.");
+        } else {
+            switch(answer.trim().toLowerCase()) {
+                case 'y' || 'yes':
+                   return true;
+                case 'n' || 'no':
+                    return false;
+                default:
+                    alert("Por favor ingrese una respuesta valida.");
+                    break;
+            }
+        }
+    }
+}
+
+validateStringPrompt = (message) => {
+    const retry = true;
+    while(retry) {
+        const answer = prompt(message);
+        if (!answer || answer === '' || answer === undefined ) {
+            alert("Por favor ingrese un texto valido.");
+        } else {
+            return answer.trim().toLowerCase();
+        }
+    }
+}
+
+validateNumberPrompt = (message) => {
+    let retry = true;
+    while(retry) {
+        const answer = parseInt(prompt(message));
+        if (isNaN(answer)) {
+            alert("Por favor ingrese un numero valido.");
+        } else {
+            return answer;
+        }
+    }
+}
+
+shouldAddProducts = () => {
+    const cart = new Cart();
+    let answer;
+    do{
+        answer = validateYesNoPrompt('Desea ingresar productos a su carrito? y / n');
+        console.log()
+        if(answer) {
+            const productName = validateStringPrompt('Ingrese nombre del producto');
+            const productPrice = validateNumberPrompt('Ingrese valor del producto');
+            const productQty = validateNumberPrompt('Ingrese cantidad del producto');
+            const product = new Product(productName, productPrice);
+            cart.addProduct(product, productQty);
+        }
+    } while (answer);
+    const products = cart.getProducts();
+    if(products.length === 0) {
+        alert('No hay productos en el carrito');
+    } else {
+        let message = 'Sus productos en el carrito son: \n';
+        let total = 0;
+        products.forEach(product => {
+            message += `${product.name}: ${product.price} * ${product.qty} = ${product.total} \n`;
+            total += product.total;
+        });
+        message +=  `TOTAL: $${total}`;
+        alert(message);
+    }
+}
+
+shouldAddProducts();
